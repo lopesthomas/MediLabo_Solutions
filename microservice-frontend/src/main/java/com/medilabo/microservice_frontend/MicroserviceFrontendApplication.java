8e85dd5a93890595d5a86ce7573d3cf75e07a88a
LOPES Thomas <thomaslopes001@gmail.com>
2025-07-23 20:13:12 +0200

    feat: Initialize microservice-gateway and microservice-patient projects with Spring Boot
    - Added microservice-gateway module with Spring Boot configuration, dependencies, and application properties.
    - Implemented main application class for microservice-gateway.
    - Created test class for microservice-gateway.
    - Added Maven wrapper scripts and properties for microservice-gateway.
    
    - Added microservice-patient module with Spring Boot configuration, dependencies, and application properties.
    - Implemented main application class for microservice-patient.
    - Created test class for microservice-patient.
    - Added Maven wrapper scripts and properties for microservice-patient.

---
+++ microservice-frontend/src/main/java/com/medilabo/microservice_frontend/MicroserviceFrontendApplication.java
@@ -0,0 +1,13 @@
+package com.medilabo.microservice_frontend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceFrontendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceFrontendApplication.class, args);
+	}
+
+}
---
+++ microservice-frontend/src/test/java/com/medilabo/microservice_frontend/MicroserviceFrontendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medilabo.microservice_frontend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceFrontendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
