3914787126e9ea40cb1fc7dcef546570a7fd4b76
LOPES Thomas <thomaslopes001@gmail.com>
2025-07-27 23:43:09 +0200

    Refactor gateway configuration: move RouteLocator bean to GatewayConfig class

--- microservice-gateway/src/main/java/com/medilabo/microservice_gateway/MicroserviceGatewayApplication.java
+++ microservice-gateway/src/main/java/com/medilabo/microservice_gateway/MicroserviceGatewayApplication.java
@@ -2,9 +2,6 @@ package com.medilabo.microservice_gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.gateway.route.RouteLocator;
-import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class MicroserviceGatewayApplication {
@@ -12,17 +9,5 @@ public class MicroserviceGatewayApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(MicroserviceGatewayApplication.class, args);
 	}
-
-	@Bean
-    public RouteLocator routeLocator(RouteLocatorBuilder builder) {
-        return builder.routes()
-			.route("patient_route", r -> r
-				.path("/patients/**")
-				.uri("http://localhost:8081"))
-			.route("frontend_route", r -> r
-				.path("/frontend/**")
-				.uri("http://localhost:8082"))
-			.build();
-    }
 
 }
--- microservice-gateway/src/main/java/com/medilabo/microservice_gateway/config/GatewayConfig.java
+++ microservice-gateway/src/main/java/com/medilabo/microservice_gateway/config/GatewayConfig.java
@@ -1,8 +1,23 @@
 package com.medilabo.microservice_gateway.config;
 
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class GatewayConfig {
+
+    @Bean
+    public RouteLocator routeLocator(RouteLocatorBuilder builder) {
+        return builder.routes()
+			.route("patient_route", r -> r
+				.path("/patients/**")
+				.uri("http://localhost:8081"))
+			.route("frontend_route", r -> r
+				.path("/frontend/**")
+				.uri("http://localhost:8082"))
+			.build();
+    }
 
 }
