046e5062ed7a73aad8aa6a9e68b4677f9c10b322
LOPES Thomas <thomaslopes001@gmail.com>
2025-08-03 12:00:51 +0200

    Add Patient view page with patient details and associated notes display

--- microservice-frontend/src/main/java/com/medilabo/microservice_frontend/controller/PatientController.java
+++ microservice-frontend/src/main/java/com/medilabo/microservice_frontend/controller/PatientController.java
@@ -6,9 +6,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
+import com.medilabo.microservice_frontend.model.Note;
 import com.medilabo.microservice_frontend.model.Patient;
+import com.medilabo.microservice_frontend.proxy.NoteProxy;
 import com.medilabo.microservice_frontend.proxy.PatientProxy;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 
 @Controller
@@ -16,6 +21,8 @@ public class PatientController {
 
     @Autowired
     private PatientProxy patientProxy;
+    @Autowired
+    private NoteProxy noteProxy;
     
     @GetMapping("/listpatients")
     public String listPatients(Model model) {
@@ -24,5 +31,13 @@ public class PatientController {
         return "ListPatients";
     }
     
+    @GetMapping("/patient/view/{id}")
+    public String getPatient(@PathVariable("id") String id, Model model) {
+        Patient patient = patientProxy.getPatientById(id);
+        model.addAttribute("patient", patient);
+        List<Note> notes = noteProxy.getNotesByPatientId(id);
+        model.addAttribute("notes", notes);
+        return "Patient";
+    }
 
 }
