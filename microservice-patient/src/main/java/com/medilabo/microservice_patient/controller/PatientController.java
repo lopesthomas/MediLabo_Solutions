7cfdb14f29f431be2c58cd66f81e4937c9abdece
LOPES Thomas <thomaslopes001@gmail.com>
2025-08-03 12:12:31 +0200

    Add delete patient endpoint and update patient handling in PatientService

--- microservice-patient/src/main/java/com/medilabo/microservice_patient/controller/PatientController.java
+++ microservice-patient/src/main/java/com/medilabo/microservice_patient/controller/PatientController.java
@@ -3,6 +3,7 @@ package com.medilabo.microservice_patient.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -26,15 +27,24 @@ public class PatientController {
         return patientService.getAllPatients();
     }
 
+    @PostMapping("/add")
+    public Patient addPatient(@RequestBody Patient patient) {
+        return patientService.savePatient(patient);
+    }
+
     @PostMapping("/update")
     public Patient updatePatient(@RequestBody Patient patient) {
-        return patientService.savePatient(patient);
+        return patientService.updatePatient(patient);
     }
 
     @GetMapping("/{id}")
     public Patient getPatientById(@PathVariable Long id) {
         return patientService.getPatientById(id);
     }
-    
+
+    @DeleteMapping("delete/{id}")
+    public void deletePatient(@PathVariable Long id) {
+        patientService.deletePatient(id);
+    }
 
 }
