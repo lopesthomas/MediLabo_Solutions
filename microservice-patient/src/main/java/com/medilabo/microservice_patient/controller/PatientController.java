5ad49eb7dfc1d25f48a7128289ba56a0cb72cb58
LOPES Thomas <thomaslopes001@gmail.com>
2025-08-03 12:05:36 +0200

    Add endpoint to retrieve patient by ID in PatientController

--- microservice-patient/src/main/java/com/medilabo/microservice_patient/controller/PatientController.java
+++ microservice-patient/src/main/java/com/medilabo/microservice_patient/controller/PatientController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -28,6 +29,11 @@ public class PatientController {
     @PostMapping("/update")
     public Patient updatePatient(@RequestBody Patient patient) {
         return patientService.savePatient(patient);
+    }
+
+    @GetMapping("/{id}")
+    public Patient getPatientById(@PathVariable Long id) {
+        return patientService.getPatientById(id);
     }
     
 
