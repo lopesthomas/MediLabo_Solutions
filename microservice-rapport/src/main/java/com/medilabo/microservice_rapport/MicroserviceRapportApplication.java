58ea1d83b9acdd1742118983c09c145c65aa06e9
LOPES Thomas <thomaslopes001@gmail.com>
2025-08-10 12:19:01 +0200

    Initialize microservice-rapport project: add Maven wrapper, POM file, application properties, main application class, and test class

---
+++ microservice-rapport/src/main/java/com/medilabo/microservice_rapport/MicroserviceRapportApplication.java
@@ -0,0 +1,13 @@
+package com.medilabo.microservice_rapport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceRapportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceRapportApplication.class, args);
+	}
+
+}
---
+++ microservice-rapport/src/test/java/com/medilabo/microservice_rapport/MicroserviceRapportApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medilabo.microservice_rapport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceRapportApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
