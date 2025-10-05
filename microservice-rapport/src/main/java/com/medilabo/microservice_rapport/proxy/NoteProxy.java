96a367d67ecb78021e13d75c86145816153c4590
LOPES Thomas <thomaslopes001@gmail.com>
2025-10-05 23:24:23 +0200

    Add security configuration with OAuth2 support and Feign client integration for patient and note services in microservice-rapport

--- microservice-rapport/src/main/java/com/medilabo/microservice_rapport/proxy/NoteProxy.java
+++ microservice-rapport/src/main/java/com/medilabo/microservice_rapport/proxy/NoteProxy.java
@@ -6,9 +6,10 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import com.medilabo.microservice_rapport.config.FeignConfig;
 import com.medilabo.microservice_rapport.model.Note;
 
-@FeignClient(name = "note-service", url = "http://microservice-gateway:8090")
+@FeignClient(name = "note-service", url = "http://microservice-gateway:8090", configuration = FeignConfig.class)
 public interface NoteProxy {
 
     @GetMapping("/api/notes/{patientId}")
--- microservice-rapport/src/main/java/com/medilabo/microservice_rapport/proxy/PatientProxy.java
+++ microservice-rapport/src/main/java/com/medilabo/microservice_rapport/proxy/PatientProxy.java
@@ -4,9 +4,10 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import com.medilabo.microservice_rapport.config.FeignConfig;
 import com.medilabo.microservice_rapport.model.Patient;
 
-@FeignClient(name = "patient-service", url = "http://microservice-gateway:8090")
+@FeignClient(name = "patient-service", url = "http://microservice-gateway:8090", configuration = FeignConfig.class)
 public interface PatientProxy {
 
     @GetMapping("/api/patients/{id}")
